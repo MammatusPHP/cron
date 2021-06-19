a714f113f90a15ecc87acbf62ddd4a754c9d3742
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-06-19 16:45:00 +0200

    Restore logging

--- src/Manager.php
+++ src/Manager.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 namespace Mammatus\Cron;
 
 use Mammatus\Cron\Generated\AbstractManager;
-use Mammatus\Cron\Worker\ActionWorkerFactory;
 use Mammatus\LifeCycleEvents\Initialize;
 use Mammatus\LifeCycleEvents\Shutdown;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use React\EventLoop\LoopInterface;
-use ReactParallel\Factory;
-use ReactParallel\Pool\Worker\Worker as WorkerPool;
+use React\Promise\PromiseInterface;
 use WyriHaximus\Broadcast\Contracts\Listener;
 use WyriHaximus\React\Mutex\Contracts\MutexInterface;
+use function React\Promise\resolve;
 
 final class Manager extends AbstractManager implements Listener
 {
@@ -34,14 +33,26 @@ final class Manager extends AbstractManager implements Listener
     public function start(Initialize $event): void
     {
         $this->logger->debug('Starting cron manager');
-        $this->cron($this->loop, $this->mutex, $this->container);
+        $this->cron($this->loop, $this->mutex);
         $this->logger->debug('Started cron manager');
     }
 
     public function stop(Shutdown $event): void
     {
         $this->logger->debug('Stopping cron manager');
-        $this->cron($this->loop, $this->mutex, $this->container)->stop();
+        $this->cron($this->loop, $this->mutex)->stop();
         $this->logger->debug('Stopped cron manager');
+    }
+
+    protected function perform(string $class): PromiseInterface
+    {
+        $this->logger->debug('Starting job: ' . $class);
+        return $this->container->get($class)->perform()->then(function ($outcome) use ($class): PromiseInterface {
+            $this->logger->debug('Job finished: ' . $class);
+
+            return resolve($outcome);
+        }, function (\Throwable $outcome) use ($class): void {
+            $this->logger->debug('Job errored: ' . $class . ' ' . (string)$outcome);
+        });
     }
 }
